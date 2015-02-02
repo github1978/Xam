1d8229b56ce5d8249b2c3e9212b9d9eed14f2d42
github1978 <umbrella1978@live.cn>
2015-02-02 22:11:31 +0800

    删除多余JDBCExcutor中多余的方法

--- xam/src/main/java/com/wisesignsoft/xam/datadriven/JDBCExecutor.java
+++ xam/src/main/java/com/wisesignsoft/xam/datadriven/JDBCExecutor.java
@@ -3,6 +3,7 @@ package com.wisesignsoft.xam.datadriven;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.sql.Statement;
 
 import com.wisesignsoft.xam.controller.TestConfigration.Source;
@@ -14,25 +15,15 @@ public class JDBCExecutor {
 	// Statement对象,可以执行SQL语句并返回结果
 	private Statement stmt;
 
-	public JDBCExecutor(Source ds) {
-		try {
-			ConnStr constr = new ConnStr(ds);
-			// 初始化JDBC驱动并让驱动加载到jvm中
-			Class.forName(constr.getDriver());
-			// 创建数据库连接
-			connection = DriverManager.getConnection(constr.getUrl(),
-					constr.getUser(), constr.getPwd());
-			// 创建Statement对象
-			stmt = connection.createStatement(ResultSet.TYPE_SCROLL_SENSITIVE,
-					ResultSet.CONCUR_UPDATABLE);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+	public JDBCExecutor(Source ds) throws Exception {
+		ConnStr constr = new ConnStr(ds);
+		Class.forName(constr.getDriver());
+		connection = DriverManager.getConnection(constr.getUrl(),
+		constr.getUser(), constr.getPwd());
+		stmt = connection.createStatement(ResultSet.TYPE_SCROLL_SENSITIVE,
+			ResultSet.CONCUR_UPDATABLE);
 	}
 
-	/*
-	 * 执行一句查询的sql
-	 */
 	public ResultSet executeQuery(String sql) {
 		try {
 			// 利用Statement对象执行参数的sql
@@ -42,26 +33,6 @@ public class JDBCExecutor {
 			e.printStackTrace();
 		}
 		return null;
-	}
-
-	// 执行单句INSERT、UPDATE 或 DELETE 语句, 如果执行INSERT时, 返回主键
-	public int executeUpdate(String sql) {
-		int result = -1;
-		try {
-			// 执行SQL语句
-			stmt.executeUpdate(sql);
-			// 获得主键
-			ResultSet rs = stmt.getGeneratedKeys();
-			while (rs.next()) {
-				// 返回最后一个主键
-				result = rs.getInt(1);
-			}
-			rs.close();
-			return result;
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		return result;
 	}
 
 	/**
@@ -80,16 +51,11 @@ public class JDBCExecutor {
 		return rs.getString(1);
 	}
 
-	public int getRsRowCount(ResultSet rs) {
+	public int getRsRowCount(ResultSet rs) throws SQLException {
 		int rowCount = 0;
-		try {
-			rs.last();
-			rowCount = rs.getRow();
-			rs.first();
-		} catch (Exception e) {
-			// TODO: handle exception
-			e.printStackTrace();
-		}
+		rs.last();
+		rowCount = rs.getRow();
+		rs.first();
 		return rowCount;
 	}
 }
